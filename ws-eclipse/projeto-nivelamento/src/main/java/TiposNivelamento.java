481e8444aa2648593b3b12685bc1a5dd9f6e5d31
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2024-02-14 17:44:43 -0300

    Criando o projeto de nivelamento

---
+++ ws-eclipse/projeto-nivelamento/src/main/java/TiposNivelamento.java
@@ -0,0 +1,15 @@
+public class TiposNivelamento {
+    public static void main(String[] args) {
+        //Principais tipo
+
+        String nome = "Jonathan";
+
+        Double altura = 1.69;
+
+        Integer numeroSorteio = 42;
+
+        Boolean usuarioBloqueado = false;
+
+        System.out.println("meu nome é " + nome + " tenho " + altura + " de altura e meu numero de sorteiro é " + numeroSorteio);
+    }
+}
